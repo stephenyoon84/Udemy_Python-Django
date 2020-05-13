dbd12151c4d6f81fcae5c74e003b832c67828600
Stephen <stephenyoon84@gmail.com>
2020-05-13 11:41:43 -0400

    exercise 1

--- 11.Python_Level_One/Part6_Exercise_Review.py
+++ 11.Python_Level_One/Part6_Exercise_Review.py
@@ -33,8 +33,8 @@ print(s[::-1])
 # Given this nested list:
 l = [3,7,[1,4,'hello']]
 # Reassign "hello" to be "goodbye"
-
-
+l[2][2] = "goodbye"
+print(l)
 ###############
 ## Problem 3 ##
 ###############
